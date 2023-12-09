891b358d2d22bb9db31518768facfe623f224303
Matt Christensen <mchristensen@moosepod.com>
2023-12-09 09:56:05 -0500

    Experimenting with day 5

--- day_5/utils.py
+++ day_5/utils.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, model_validator
 
 class P(BaseModel):
@@ -12,9 +13,28 @@ class R(BaseModel):
     b: int
 
     def overlap(self, r: 'R') -> bool:
-        return max(self.a, r.a) <= min(self.b, r.b) 
+        return max(self.a, r.a) <= min(self.b, r.b)
+
+    def intersection(self, r: 'R') -> Optional['R']:
+        new_a = max(self.a, r.a)
+        new_b = min(self.b, r.b)
+        if new_a <= new_b:
+            return R(a=new_a,b=new_b)
+        return None
+
+    def __sub__(self, value) -> Optional['R']:
+        if type(value) == R:
+            return self.intersection(value)
+
+        raise ValueError("Can only subtract a R from an R")
+
+    def __add__(self, value) -> 'R':
+        return R(a=self.a + value, b=self.b+value)
 
     def __contains__(self, value) -> bool:
+        if type(value) == R:
+            return self.overlap(value)
+            
         try:
             x = int(value)
             return x >= self.a and x <= self.b
