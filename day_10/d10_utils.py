c2c72ec3b74f1f7829b278c9946ce23340430c34
Matt Christensen <mchristensen@moosepod.com>
2023-12-10 08:04:37 -0500

    Day 10 data loaded

---
+++ day_10/d10_utils.py
@@ -0,0 +1,57 @@
+from typing import Optional
+from pydantic import BaseModel, model_validator
+
+class P(BaseModel):
+    """ A point """
+    x: int = 0
+    y: int = 0
+    z: int = 0
+
+    def __hash__(self):
+        return hash(f'{self.x}x{self.y}x{self.z}')
+
+class S(BaseModel):
+    """A size. Not really different than point, but keeps var names separate"""
+    width: int
+    height: int
+
+class R(BaseModel):
+    """ A range with a/b inclusive""" 
+    a: int 
+    b: int
+
+    def overlap(self, r: 'R') -> bool:
+        return max(self.a, r.a) <= min(self.b, r.b)
+
+    def intersection(self, r: 'R') -> Optional['R']:
+        new_a = max(self.a, r.a)
+        new_b = min(self.b, r.b)
+        if new_a <= new_b:
+            return R(a=new_a,b=new_b)
+        return None
+
+    def __sub__(self, value) -> Optional['R']:
+        if type(value) == R:
+            return self.intersection(value)
+
+        raise ValueError("Can only subtract a R from an R")
+
+    def __add__(self, value) -> 'R':
+        return R(a=self.a + value, b=self.b+value)
+
+    def __contains__(self, value) -> bool:
+        if type(value) == R:
+            return self.overlap(value)
+            
+        try:
+            x = int(value)
+            return x >= self.a and x <= self.b
+        except ValueError:
+            return False
+        
+    @model_validator(mode='after')
+    def check_ordering(self) -> 'R':
+        if self.a > self.b:
+            raise ValueError(f"a [{self.a}] is greater than b [{self.b}]")
+
+        return self
