6b135defe563a0f00d155fd07588d1c73b369c41
Matt Christensen <mchristensen@moosepod.com>
2023-12-18 17:33:07 -0500

    Work on pathfinding experiments

--- day_17/d17_utils.py
+++ day_17/d17_utils.py
@@ -5,24 +5,23 @@ class P(BaseModel):
     """ A point """
     x: int = 0
     y: int = 0
-    z: int = 0
 
     def __hash__(self):
-        return hash(f'{self.x}x{self.y}x{self.z}')
+        return hash(f'{self.x}x{self.y}')
 
     def __add__(self, value):
         # Assume adding a P
-        return P(x=self.x+value.x, y=self.y+value.y, z=self.z+value.z)
+        return P(x=self.x+value.x, y=self.y+value.y)
 
     def __mul__(self,value):
         # Assume multiplying by int
-        return P(x=self.x*value, y=self.y*value, z=self.z*value)
+        return P(x=self.x*value, y=self.y*value)
     
     def __eq__(self, value):
-        return self.x == value.x and self.y == value.y and self.z == value.z
+        return self.x == value.x and self.y == value.y
     
     def clone(self) -> 'P':
-        return P(x=self.x,y=self.y,z=self.z)
+        return P(x=self.x,y=self.y)
         
 class S(BaseModel):
     """A size. Not really different than point, but keeps var names separate"""
@@ -73,10 +72,42 @@ class R(BaseModel):
 
         return self
 
+RIGHT=P(x=1,y=0)
+LEFT=P(x=-1,y=0)
+UP=P(x=0,y=-1)
+DOWN=P(x=0,y=1)
 
 class Grid(BaseModel):
     size: S
     cells: dict[P, int]
+
+    def is_blocked(self, p: P) -> bool:
+        return self.cells.get(p,0) != 0
+    
+    def bfs(self, start: P, end: P) -> list[P]:
+        frontier = [start]
+        reached = {}
+
+        while frontier:
+            p = frontier.pop()
+            if p == end:
+                return [p]
+            if p == start or (not self.is_blocked(p) and not reached.get(p)):
+                reached[p] = True
+                frontier.append(p + UP)
+                frontier.append(p + DOWN)
+                frontier.append(p + LEFT)
+                frontier.append(p + RIGHT)
+
+        return []
+            
+    def find_first_value(self, v: int) -> P | None:
+        for y in range(0,self.size.height):
+            for x in range(0,self.size.width):
+                p = P(x=x,y=y)
+                if self.cells.get(p) == v:
+                    return p
+        return None
     
     def in_bounds(self,p: P) -> bool:
         return p.x >=0 and p.y >=0 and p.x < self.size.width and p.y < self.size.height
@@ -92,13 +123,15 @@ def load_grid_from_str(s: str) -> Grid:
   cells = {}
   width,height = 0,0
 
-  for y, row in enumerate(s.split("\n")):
-      if row:
+  y = 0
+  for row in s.split("\n"):
+      if row.strip():
         width = len(row)
         height +=1 
         for x, col in enumerate(row.strip()):
           p = P(x=x,y=y)
           cells[p] = int(col)
+        y+=1
 
   return Grid(size=S(width=width,height=height),cells=cells)
 
